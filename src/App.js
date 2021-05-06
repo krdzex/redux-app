6ba10b6f18d6aa2f76d47c68532ec04599d90145
Krsto Kostic <kostic.krsto@gmail.com>
2021-05-06 17:16:23 +0200

    Adding redux project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import SongDetail from "./components/SongDetail";
+import SongList from "./components/SongList";
+
+
+const App = () => {
+    return <div>
+        <div className="left">
+            <SongList />
+        </div>
+        <div className="right panel">
+            <SongDetail />
+        </div>
+    </div>
+}
+
+export default App;
---
+++ src/components/SongDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+    if (!song) return <div>Please select a song</div>
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>Title: {song.title}</p>
+            <p>Duration: {song.duration}</p>
+        </div>
+    );
+}
+
+const mapsStateToProps = state => {
+    return { song: state.selectedSong }
+
+}
+
+export default connect(mapsStateToProps)(SongDetail);
